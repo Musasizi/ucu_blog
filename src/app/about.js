56414fbf754ba9c38e178bd2b5270e399c9375dc
Musasizi Kenneth <24485588+Musasizi@users.noreply.github.com>
2024-11-07 08:45:41 +0300

    Added more components

---
+++ src/app/about.js
@@ -0,0 +1,16 @@
+
+
+function About() {
+
+    function goToNextPage() { }
+
+    return (
+        <div>
+            <h1>About is here!</h1>
+
+
+        </div>
+    );
+}
+
+export default About;
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Title from "../components/Title";
+
 import Content from "../components/Content";
 
 function Home() {
